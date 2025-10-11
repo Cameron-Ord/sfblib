5e36a4ee77c67751dff0d26f5cc889937311f3ec
Cameron Ord <cameron.ian.ronald.ord@outlook.com>
2025-10-10 20:48:52 -0600

    more logic fixes and formatg

--- sfblib/allocator.c
+++ sfblib/allocator.c
@@ -17,42 +17,42 @@ sfb_framebuffer *sfb_buffer_alloc(const int width, const int height) {
   return fb_obj;
 }
 
-sfb_obj *sfb_create_rect(int x, int y, int w, int h, uint32_t colour){
-    sfb_obj *obj = calloc(1, sizeof(sfb_obj));
-    if(!obj){
-      fprintf(stderr, "!calloc()->%s\n", strerror(errno));
-      return NULL;
-    }
-
-    obj->pixels = malloc(w * h * sizeof(uint32_t));
-    if(!obj->pixels){
-      fprintf(stderr, "!malloc()->%s\n", strerror(errno));
-      free(obj);
-      return NULL;
-    }
-    memset(obj->pixels, colour, w * h * sizeof(uint32_t));
-
-    sfb_mat3x3 delta = sfb_identity();
-    sfb_mat3x3 object_matrix = sfb_identity();
-    delta = sfb_translate(delta, x, y);
-
-    obj->mat = sfb_mmult(&delta, &object_matrix);
-    obj->w = w;
-    obj->h = h;
-    obj->type = SFB_RECT;
-
-    return obj;
-}
-
-sfb_obj *sfb_rect_from_sprite(const int w, const int h, uint32_t *spr){
+sfb_obj *sfb_create_rect(int x, int y, int w, int h, uint32_t colour) {
   sfb_obj *obj = calloc(1, sizeof(sfb_obj));
-  if(!obj){
+  if (!obj) {
     fprintf(stderr, "!calloc()->%s\n", strerror(errno));
     return NULL;
   }
 
   obj->pixels = malloc(w * h * sizeof(uint32_t));
-  if(!obj->pixels){
+  if (!obj->pixels) {
+    fprintf(stderr, "!malloc()->%s\n", strerror(errno));
+    free(obj);
+    return NULL;
+  }
+  memset(obj->pixels, colour, w * h * sizeof(uint32_t));
+
+  sfb_mat3x3 delta = sfb_identity();
+  sfb_mat3x3 object_matrix = sfb_identity();
+  delta = sfb_translate(delta, x, y);
+
+  obj->mat = sfb_mmult(&delta, &object_matrix);
+  obj->w = w;
+  obj->h = h;
+  obj->type = SFB_RECT;
+
+  return obj;
+}
+
+sfb_obj *sfb_rect_from_sprite(const int w, const int h, uint32_t *spr) {
+  sfb_obj *obj = calloc(1, sizeof(sfb_obj));
+  if (!obj) {
+    fprintf(stderr, "!calloc()->%s\n", strerror(errno));
+    return NULL;
+  }
+
+  obj->pixels = malloc(w * h * sizeof(uint32_t));
+  if (!obj->pixels) {
     fprintf(stderr, "!malloc()->%s\n", strerror(errno));
     free(obj);
     return NULL;
@@ -62,7 +62,7 @@ sfb_obj *sfb_rect_from_sprite(const int w, const int h, uint32_t *spr){
   sfb_mat3x3 delta = sfb_identity();
   sfb_mat3x3 object_matrix = sfb_identity();
   delta = sfb_translate(delta, 0, 0);
- 
+
   obj->mat = sfb_mmult(&delta, &object_matrix);
   obj->w = w;
   obj->h = h;
@@ -72,18 +72,17 @@ sfb_obj *sfb_rect_from_sprite(const int w, const int h, uint32_t *spr){
 }
 
 void sfb_free_framebuffer(sfb_framebuffer *f) {
-  if(f && f->data){
+  if (f && f->data) {
     free(f->data);
     free(f);
   }
   f = NULL;
 }
 
-void sfb_free_obj(sfb_obj *o){
-  if(o && o->pixels){
+void sfb_free_obj(sfb_obj *o) {
+  if (o && o->pixels) {
     free(o->pixels);
     free(o);
   }
   o = NULL;
 }
-
--- sfblib/buf_write.c
+++ sfblib/buf_write.c
@@ -6,54 +6,65 @@ void sfb_fb_clear(sfb_framebuffer *const buffer, uint32_t clear_colour) {
   }
 }
 
-void sfb_write_obj_rect(const sfb_obj *const obj, sfb_framebuffer *const buffer) {
+void sfb_write_obj_rect(const sfb_obj *const obj,
+                        sfb_framebuffer *const buffer) {
   const int y0 = obj->mat.m5;
   const int x0 = obj->mat.m2;
 
-  for(int dy = 0; dy < obj->h; dy++){
+  for (int dy = 0; dy < obj->h; dy++) {
     const int y = y0 + dy;
-    if(y < 0 || y > buffer->h) continue; 
-    for(int dx = 0; dx < obj->w; dx++){
+    if (y < 0 || y >= buffer->h)
+      continue;
+
+    for (int dx = 0; dx < obj->w; dx++) {
       const int x = x0 + dx;
-      if(x < 0 || x > buffer->w) continue;
-      sfb_put_pixel(x, y, buffer->data, buffer->w, buffer->h, obj->pixels[dy * obj->w + dx]);
+      if (x < 0 || x >= buffer->w)
+        continue;
+
+      sfb_put_pixel(x, y, buffer->data, buffer->w, buffer->h,
+                    obj->pixels[dy * obj->w + dx]);
     }
   }
 }
 
-void sfb_write_rect_generic(int x0, int y0, int w0, int h0, uint32_t colour, sfb_framebuffer *const buffer){
-  for(int i = 0, y = y0; y < buffer->h && i < h0; i++, y++){
-    for(int j = 0, x = x0; x < buffer->w && j < w0; j++, x++){
+void sfb_write_rect_generic(int x0, int y0, int w0, int h0, uint32_t colour,
+                            sfb_framebuffer *const buffer) {
+  for (int i = 0, y = y0; y < buffer->h && i < h0; i++, y++) {
+    for (int j = 0, x = x0; x < buffer->w && j < w0; j++, x++) {
       sfb_put_pixel(x, y, buffer->data, buffer->w, buffer->h, colour);
     }
   }
 }
 
-void sfb_write_circle_generic(const int xc, const int yc, uint32_t colour, sfb_framebuffer *const buffer, const int radius){
+void sfb_write_circle_generic(const int xc, const int yc, uint32_t colour,
+                              sfb_framebuffer *const buffer, const int radius) {
   const int xstart = xc - radius;
   const int ystart = yc - radius;
   const int xend = xc + radius;
   const int yend = yc + radius;
 
-  for(int y = ystart; y <= yend; y++){
-    if(y >= 0 && y < buffer->h){
-      for(int x = xstart; x <= xend; x++){
-        if(x >= 0 && x < buffer->w){
-          const int dx = x - xc;
-          const int dy = y - yc;
-          if(dx*dx + dy*dy <= radius*radius){
-            sfb_put_pixel(x, y, buffer->data, buffer->w, buffer->h, colour);
-          }
-        }
+  for (int y = ystart; y <= yend; y++) {
+    if (!(y >= 0 && y < buffer->h))
+      continue;
+
+    for (int x = xstart; x <= xend; x++) {
+      if (!(x >= 0 && x < buffer->w))
+        continue;
+
+      const int dx = x - xc;
+      const int dy = y - yc;
+      if (dx * dx + dy * dy <= radius * radius) {
+        sfb_put_pixel(x, y, buffer->data, buffer->w, buffer->h, colour);
       }
     }
   }
 }
 
-void sfb_put_pixel(const int x, const int y, uint32_t *const buf, const int w, const int h, uint32_t colour){
+void sfb_put_pixel(const int x, const int y, uint32_t *const buf, const int w,
+                   const int h, uint32_t colour) {
   const int l = y * w + x;
   const int max = w * h;
-  if(l < max && l >= 0){
+  if (l < max && l >= 0) {
     buf[l] = colour;
   }
 }
